ef7b73fedaf526f151b1715c04c38706a4d31633
Chin Kuay <kuaychin@gmail.com>
2020-04-14 15:01:16 -0500

    final

--- constructors.js
+++ constructors.js
@@ -57,19 +57,16 @@ function Car(make, model, year) {
   Find the average between the old rating and the new rating.
   Change the rating property to become this new number and return the updated rating.
 */
-
 function Movie(name, genre, rating) {
   this.name = name;
   this.genre = genre;
   this.rating = rating;
 }
-
 // Code here
 Movie.prototype.changeRating = function (num) {
   let avg = (this.rating + num) / 2;
   return avg;
 };
-
 ////////// PROBLEM 5 //////////
 // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.
 
@@ -82,27 +79,22 @@ function User(name, age, email, savedPosts) {
   this.email = email;
   this.savedPosts = savedPosts;
 }
-
 User.prototype.addSavedPost = function (id, title, rating) {
   this.savedPosts.push({ id: id, title: title, rating: rating });
 };
 ////////// PROBLEM 6 //////////
 // You will be using the constructor function you just created in problem 5.
 // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
-
 // Code here
 User.prototype.removeSavedPost = function (id) {
   let i = this.savedPosts.findIndex((el) => el.id == id);
   this.savedPosts.splice(i, 1);
 };
-
 ////////// PROBLEM 7 //////////
 // You will continue to use the constructor function you created in problem 5.
 // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
-
 // Code here
 User.prototype.changePostRating = function (id, rating) {
-  // let i = this.savedPosts.indexOf(id); //does not work with objects in array
   let i = this.savedPosts.findIndex((el) => el.id == id);
   this.savedPosts[i].rating = rating;
 };
