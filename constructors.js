846bd7a24d3c1bf26880dd2234b55830fe28a513
Chin Kuay <kuaychin@gmail.com>
2020-04-14 14:04:37 -0500

    start constructors

--- constructors.js
+++ constructors.js
@@ -6,16 +6,16 @@
 
   You can refresh the page at any time to re-run all the tests.
 */
-
 ////////// PROBLEM 1 //////////
-
 /*
   Create a constructor function called CarFactory that will build car objects with make and model properties.
   The values for the make and model will come from two parameters, make and model (in that order).
 */
-
 // Code here
-
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -25,22 +25,18 @@ function Employee(name, email, hireDate) {
   this.email = email;
   this.hireDate = hireDate;
 }
-
 // Do not edit the code above.
-
 /*
   Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
   Assign the result of the invocation to a variable called bob.
 */
-
 // Code here
-
+let bob = new Employee("Bob", "bob@gmail.com", "01-02-98");
 ////////// PROBLEM 3 //////////
-
 // Do not edit the code below.
 
-var prius = new Car('Toyota', 'Prius', 2011);
-var mustang = new Car('Ford', 'Mustang', 2013);
+var prius = new Car("Toyota", "Prius", 2011);
+var mustang = new Car("Ford", "Mustang", 2013);
 prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
 
@@ -57,7 +53,15 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 */
 
 // Code here
-
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function () {
+    return (this.move += 10);
+  };
+}
 ////////// PROBLEM 4 //////////
 
 /*
